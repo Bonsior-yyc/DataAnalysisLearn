cbc99465fded9955efff08f4bbb5ad77feaa46b4
颜聿辰 <740987012@qq.com>
2020-02-03 15:36:23 +0800

    update

--- WuEnda/Classification.py
+++ WuEnda/Classification.py
@@ -4,13 +4,13 @@ import math
 
 x = np.linspace(1, 10, 100)
 y = np.linspace(1, 10, 100)
-x_in, y_in, z_in, x_i, y_i = [],[],[],[],[]
+x_in, y_in, z_in, x_i, y_i = [], [], [], [], []
 for p in x:
     for q in y:
         if (p + q) % 0.3 != 0:
             x_in.append(p)
             y_in.append(q)
-            if 2*p + 3*q >= 25 + np.random.randint(-5, 5, size=1):
+            if 2 * p + 3 * q >= 25 + np.random.randint(-5, 5, size=1):
                 z_in.append(1)
             else:
                 z_in.append(0)
@@ -19,7 +19,7 @@ for p in x:
         x_i.append(p)
         y_i.append(q)
 size = len(z_in)
-inp = [np.ones(len(z_in)),  x_in, y_in]
+inp = [np.ones(len(z_in)), x_in, y_in]
 ip = np.array(inp)
 print(ip)
 th = np.array([0, 0, 0])
@@ -43,7 +43,7 @@ class HypothesisClassification(object):
     def calculate(self, i):
         trans = np.dot(self.theta, i)
         for i in range(len(trans)):
-            t = 1 / (1 + pow(math.e,(-1 * trans[i])))
+            t = 1 / (1 + pow(math.e, (-1 * trans[i])))
             trans[i] = t
         return trans
 
@@ -56,7 +56,7 @@ class HypothesisClassification(object):
             plt.figure(2)
             plt.cla()
             plt.scatter(x_in, y_in, color=col)
-            plt.scatter(i[1], i[2], color=np.where(self.calculate(i)>0.5, "green", "yellow"))
+            plt.scatter(i[1], i[2], color=np.where(self.calculate(i) > 0.5, "green", "yellow"))
             plt.pause(0.1)
             self.debug(i, j)
 
@@ -67,9 +67,3 @@ class HypothesisClassification(object):
 
 hypo1 = HypothesisClassification(3, len(z_in), 0.15)
 hypo1.iteration(inp, z_in)
-
-
-
-
-
-
